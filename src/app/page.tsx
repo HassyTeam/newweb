048feb63021a7b400dc0d2b75b56991ea5864ad1
xilliam <78660109+realxilliam@users.noreply.github.com>
2024-08-03 19:57:46 +0300

    idk sum

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,9 +20,9 @@ export default function Home() {
           {funnyImage ? <Image alt="pöö" src="/img/hassy.png" fill /> : ""}
           <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
             {
+              
               /*
-
-              Announcement alert
+               Announcement alert 
 
               <a href="#" className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
                   <span className="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">Important</span> <span className="text-sm font-medium">SOME SHIT HAPPENED</span> 
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,4 +1,6 @@
+import { link } from "fs";
 import Link from "next/link";
+import { UrlObject } from "url";
 
 export default function Projects() {
     return (
@@ -9,88 +11,54 @@ export default function Projects() {
                     Our projects
                 </h2>
                 <p className="mt-4 text-base font-normal text-gray-500 sm:text-xl dark:text-gray-400">
-                    Crafted with skill and care to help our clients grow their business!
+                    Certified HassyTeam quality
                 </p>
                 </div>
 
                 <div className="grid grid-cols-1 mt-12 text-center sm:mt-16 gap-x-20 gap-y-12 sm:grid-cols-2 lg:grid-cols-3">
-                    <div className="bg-assy23 rounded-xl bg-center bg-no-repeat">
-                        <div className="space-y-4 glass p-4 transition duration-500 hover:scale-110 h-full">
-                            <span
-                            className="bg-gray-100 text-gray-900 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
-                                Demo
-                            </span>
-                            <h3 className="text-2xl font-bold leading-tight text-white">
-                                Assembly &apos;23 demo
-                            </h3>
-                            <p className="text-lg font-normal text-gray-400">
-                                really freaking bad
-                            </p>
-                            <Link href="https://demozoo.org/productions/327573/" title=""
-                            className="text-white bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700"
-                            role="button">
-                                Demozoo
-                                <svg aria-hidden="true" className="w-5 h-5 ml-2 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                                    fill="currentColor">
-                                    <path fill-rule="evenodd"
-                                    d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                                    clip-rule="evenodd" />
-                                </svg>
-                            </Link>
+                    {[
+                        {
+                            type: "Demo",
+                            title: "Paneeli",
+                            description: "Axsoterin paneeli on erittäin tehokas ja helppokäyttöinen. Paneeli näyttää reaaliajassa tietoja palvelimen resurssien kulutuksesta.",
+                            link: "https://example.com",
+                            linklabel: "Text"
+                        },
+                        {
+                            type: "Demo",
+                            title: "Paneeli",
+                            description: "Axsoterin paneeli on erittäin tehokas ja helppokäyttöinen. Paneeli näyttää reaaliajassa tietoja palvelimen resurssien kulutuksesta.",
+                            link: "https://example.com",
+                            linklabel: "Text"
+                        }
+                    ].map((feature, index) => (
+                        <div className="bg-assy23 rounded-xl bg-center bg-no-repeat">
+                            
+                            <div className="space-y-4 glass p-4 transition duration-500 hover:scale-110 h-full">
+                                <span
+                                className="bg-gray-100 text-gray-900 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+                                    {feature.type}
+                                </span>
+                                <h3 className="text-2xl font-bold leading-tight text-white">
+                                    {feature.title}
+                                </h3>
+                                <p className="text-lg font-normal text-gray-400">
+                                    {feature.description}
+                                </p>
+                                <Link href={feature.link} title=""
+                                className="text-white bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700"
+                                role="button">
+                                    {feature.linklabel}
+                                    <svg aria-hidden="true" className="w-5 h-5 ml-2 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                        fill="currentColor">
+                                        <path fill-rule="evenodd"
+                                        d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd" />
+                                    </svg>
+                                </Link>
+                            </div>
                         </div>
-                    </div>
-                    
-                    <div className="bg-assy23 rounded-xl bg-center bg-no-repeat">
-                        <div className="space-y-4 glass p-4 transition duration-500 hover:scale-110 h-full">
-                            <span
-                            className="bg-gray-100 text-gray-900 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
-                                Demo
-                            </span>
-                            <h3 className="text-2xl font-bold leading-tight text-white">
-                                Assembly &apos;23 demo
-                            </h3>
-                            <p className="text-lg font-normal text-gray-400">
-                                really freaking bad
-                            </p>
-                            <Link href="https://demozoo.org/productions/327573/" title=""
-                            className="text-white bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700"
-                            role="button">
-                                Demozoo
-                                <svg aria-hidden="true" className="w-5 h-5 ml-2 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                                    fill="currentColor">
-                                    <path fill-rule="evenodd"
-                                    d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                                    clip-rule="evenodd" />
-                                </svg>
-                            </Link>
-                        </div>
-                    </div>
-
-                    <div className="bg-assy23 rounded-xl bg-center bg-no-repeat">
-                        <div className="space-y-4 glass p-4 transition duration-500 hover:scale-110 h-full">
-                            <span
-                            className="bg-gray-100 text-gray-900 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
-                                Demo
-                            </span>
-                            <h3 className="text-2xl font-bold leading-tight text-white">
-                                Assembly &apos;23 demo
-                            </h3>
-                            <p className="text-lg font-normal text-gray-400">
-                                really freaking bad
-                            </p>
-                            <Link href="https://demozoo.org/productions/327573/" title=""
-                            className="text-white bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700"
-                            role="button">
-                                Demozoo
-                                <svg aria-hidden="true" className="w-5 h-5 ml-2 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                                    fill="currentColor">
-                                    <path fill-rule="evenodd"
-                                    d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                                    clip-rule="evenodd" />
-                                </svg>
-                            </Link>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </section>
